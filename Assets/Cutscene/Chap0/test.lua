68899bfdb73ab65f1ddf6e6177763eda0433ea63
hy <87596507+Snow0406@users.noreply.github.com>
2025-08-12 08:21:45 +0900

    feat: LuaFlowHotReloadWindow

--- Assets/Cutscene/Chap0/test.lua
+++ Assets/Cutscene/Chap0/test.lua
@@ -6,22 +6,6 @@ local latte = get("StrawBerryLatte")
 
 function playCutscene()
     log("Test Cutscene Start")
-    
-    wait(1.0)
-    
-    tg1:action():execAsync("TestIntActionAsync", 1000)
-    
-    wait(2.0)
-    
-    tg1:camera():follow(0.03, true)
-    tg2:camera():follow(0.03, true)
-
-    latte:move():speed(6.0)
-    latte:move():to(19, 3.5)
-
-    latte:move():toSync(19, -3.5, 2)
-    latte:camera():follow(0.1, false)
-    latte:action():exec("TestVoidAction1")
-    
-    log("Test Cutscene End")
+    wait(8.0)
+    log("Test Cutscene End !")
 end
